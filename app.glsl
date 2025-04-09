d1a0108bbf2e9b3e72ee3b1ee0ae6ad665a8a491
Vampire.js <vampirejs.dev@gmail.com>
2025-04-09 16:45:58 +0530

    more colors!

--- app.glsl
+++ app.glsl
@@ -10,30 +10,38 @@ vec2 complexMul(vec2 a, vec2 b) {
     );
 }
 
-
 vec2 mandlebrot(vec2 z , vec2 c){
-return complexMul(z,z) + c ;
+    return complexMul(z,z) + c;
 }
 
 void main() {
-    float zoom = 2.2;  
-vec2 center = vec2(-.0, .0);
+    float zoom = 1.4;  
+    vec2 center = vec2(-1.0, 0.0);
 
-vec2 uv = gl_FragCoord.xy / u_resolution;
-vec2 aspect = vec2(u_resolution.x / u_resolution.y, 1.0);
+    vec2 uv = gl_FragCoord.xy / u_resolution;
+    vec2 aspect = vec2(u_resolution.x / u_resolution.y, 1.0);
+    vec2 c = (uv - 0.5) * zoom * aspect + center;
 
-vec2 c = (uv - 0.5) * zoom * aspect + center;
+    vec2 z = vec2(0.0);
+    const int max_iter = 100;
+    float iterCount = float(max_iter);
 
-
-
-    vec2 z0 = vec2(0,0);
-
-    for(int i=0; i<100; i++){
-        vec2 new = mandlebrot(z0, c);
-        z0 = new;
+    for(int i = 0; i < max_iter; i++) {
+        z = mandlebrot(z, c);
+        if(length(z) > 2.0) {
+            iterCount = float(i);
+            break;
+        }
+    }
+    
+    float t = iterCount / float(max_iter);
+    if (iterCount < float(max_iter)) {
+        float col = cos(u_time)*(iterCount - length(z));
+        t = col / float(max_iter);
+    } else {
+        t = 0.0;
     }
 
- 
-    vec3 color = (length(z0) > 2.) ? vec3(1,1,1) : vec3(0.);
-    gl_FragColor = vec4(color, 1.);
+    vec3 color = vec3(0.5 + 0.5 * cos(6.2831 * (vec3(0.0, 0.33, 0.66) + t)));
+    gl_FragColor = vec4(color, 1.0);
 }
